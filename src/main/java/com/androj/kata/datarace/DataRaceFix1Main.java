066fe99277e3bdb0e95810781f11f86c7257035f
androj <andrzej@deep-forecast.com>
2020-04-08 09:39:43 +0200

    data race

---
+++ src/main/java/com/androj/kata/datarace/DataRaceFix1Main.java
@@ -0,0 +1,40 @@
+package com.androj.kata.datarace;
+
+public class DataRaceFix1Main {
+    public static void main(String[] args) {
+
+        SharedData sharedData = new SharedData();
+
+        Thread incrementor = new Thread(() ->{
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedData.increment();
+            }
+        });
+
+        Thread checker = new Thread(()->{
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedData.checkDataConsistency();
+            }
+        });
+
+        incrementor.start();
+        checker.start();
+
+    }
+
+    public static class SharedData{
+        private int x = 0;
+        private volatile int y = 0;
+
+        public void increment(){
+            x++;
+            y++;
+        }
+
+        public void checkDataConsistency(){
+            if(y>x){
+                System.out.println(" y > x : Data race detected");
+            }
+        }
+    }
+}
