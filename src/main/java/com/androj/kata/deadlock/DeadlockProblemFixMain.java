9a9ba662ae3824eb114d4ba0ac9b1f5596b8eca8
androj <andrzej@deep-forecast.com>
2020-04-09 08:15:49 +0200

    deadlock problem and possible fix

---
+++ src/main/java/com/androj/kata/deadlock/DeadlockProblemFixMain.java
@@ -0,0 +1,87 @@
+package com.androj.kata.deadlock;
+
+import java.util.Random;
+
+public class DeadlockProblemFixMain {
+
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        new Thread(new TrainA(intersection)).start();
+        new Thread(new TrainB(intersection)).start();
+    }
+
+    public static class TrainA implements Runnable{
+
+        private final Intersection intersection;
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(new Random().nextInt(5));
+            } catch (InterruptedException e) {
+            }
+            while (true) {
+                intersection.passRailA();
+            }
+        }
+    }
+
+    public static class TrainB implements Runnable{
+
+        private final Intersection intersection;
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(new Random().nextInt(5));
+            } catch (InterruptedException e) {
+            }
+            while (true) {
+                intersection.passRailB();
+            }
+        }
+    }
+
+
+    public static class Intersection {
+        Object railA = new Object();
+        Object railB = new Object();
+
+        public void passRailA() {
+            synchronized (railA) {
+                System.out.println(Thread.currentThread().getName()+" locked rail A to pass safely");
+                synchronized (railB) {
+                    System.out.println(Thread.currentThread().getName()+" passing");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+
+
+        public void passRailB() {
+            //Fix : changed the order of locking to be the same in both methods.
+            // If the threads will try to acquire locks in the same order then the deadlock will not happen
+            synchronized (railA) {
+                System.out.println(Thread.currentThread().getName()+" locked rail A to pass safely");
+                synchronized (railB) {
+                    System.out.println(Thread.currentThread().getName()+" passing");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/androj/kata/deadlock/DeadlockProblemMain.java
@@ -0,0 +1,85 @@
+package com.androj.kata.deadlock;
+
+import java.util.Random;
+
+public class DeadlockProblemMain {
+
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        new Thread(new TrainA(intersection)).start();
+        new Thread(new TrainB(intersection)).start();
+    }
+
+    public static class TrainA implements Runnable{
+
+        private final Intersection intersection;
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(new Random().nextInt(5));
+            } catch (InterruptedException e) {
+            }
+            while (true) {
+                intersection.passRailA();
+            }
+        }
+    }
+
+    public static class TrainB implements Runnable{
+
+        private final Intersection intersection;
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(new Random().nextInt(5));
+            } catch (InterruptedException e) {
+            }
+            while (true) {
+                intersection.passRailB();
+            }
+        }
+    }
+
+
+    public static class Intersection {
+        Object railA = new Object();
+        Object railB = new Object();
+
+        public void passRailA() {
+            synchronized (railB) {
+                System.out.println(Thread.currentThread().getName()+" locked rail B to pass safely");
+                synchronized (railA) {
+                    System.out.println(Thread.currentThread().getName()+" passing");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+
+
+        public void passRailB() {
+            synchronized (railA) {
+                System.out.println(Thread.currentThread().getName()+" locked rail A to pass safely");
+                synchronized (railB) {
+                    System.out.println(Thread.currentThread().getName()+" passing");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+    }
+}
