6b7d215ba589f14b6e4915cd43b2b5d1e624542d
androj <andrzej@deep-forecast.com>
2020-04-12 19:21:43 +0200

    single produer consumer bad design

--- src/main/java/com/androj/kata/multithreading/semaphor/producerconsumersingle/Main.java
+++ src/main/java/com/androj/kata/multithreading/semaphor/producerconsumersingle/Main.java
@@ -1,4 +1,22 @@
 package com.androj.kata.multithreading.semaphor.producerconsumersingle;
 
+import java.time.LocalDateTime;
+
 public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        ProducerConsumerSingle<LocalDateTime> producerConsumerSingle = new ProducerConsumerSingle<>(
+                LocalDateTime::now,
+                System.out::println
+        );
+
+        producerConsumerSingle.start();
+
+
+        //Thread.sleep(2000);
+
+        //producerConsumerSingle.stop();
+
+
+    }
 }
--- src/main/java/com/androj/kata/multithreading/semaphor/producerconsumersingle/ProducerConsumerSingle.java
+++ src/main/java/com/androj/kata/multithreading/semaphor/producerconsumersingle/ProducerConsumerSingle.java
@@ -1,4 +1,72 @@
 package com.androj.kata.multithreading.semaphor.producerconsumersingle;
 
-public class ProducerConsumerSingle {
+import java.util.concurrent.Semaphore;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ProducerConsumerSingle<T> {
+    private final Semaphore full = new Semaphore(0);
+    private final Semaphore empty = new Semaphore(1);
+    private T item = null;
+
+    private final Thread producingThread;
+    private final Thread consumingThread;
+
+    private volatile boolean keepProducing = true;
+    private volatile boolean keepConsuming = true;
+
+    public ProducerConsumerSingle(Supplier<T> producingFunction, Consumer<T> consumingFunction) {
+
+        this.producingThread = new Thread(() -> {
+            while (keepProducing) {
+                try {
+                    //Simulate some hard work
+                    Thread.sleep(1000);
+                    empty.acquire();
+                } catch (InterruptedException e) {
+                    System.out.println("Producer interrupted");
+                    return;
+                }
+                item = producingFunction.get();
+                full.release();
+            }
+        });
+
+        this.consumingThread = new Thread(() -> {
+            while (keepConsuming) {
+                try {
+                full.acquire();
+                } catch (InterruptedException e) {
+                    System.out.println("Consumer interrupted");
+                    return;
+                }
+                consumingFunction.accept(item);
+                item = null;
+                empty.release();
+            }
+        });
+
+    }
+
+    public void start() {
+        this.producingThread.start();
+        this.consumingThread.start();
+    }
+
+    public void stop() {
+        this.keepProducing = false;
+        this.keepConsuming = false;
+
+        try {
+            this.producingThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.consumingThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
